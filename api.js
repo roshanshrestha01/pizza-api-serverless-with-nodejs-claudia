10416978fc9356f6dde29561eff1d9714410b217
Roshan Shrestha <roshanshrestha01@gmail.com>
2021-10-28 11:20:20 +0545

    Pizza serverless application initial setup and deploy with claudia

---
+++ api.js
@@ -0,0 +1,13 @@
+const Api = require('claudia-api-builder');
+const api = new Api();
+
+api.get('/pizzas', () => {
+  return [
+    'Capricciosa',
+    'Quattro Formaggi',
+    'Napoletana',
+    'Margherita',
+  ];
+})
+
+module.exports = api;
