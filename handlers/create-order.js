9421e17063a508e3f35bf243fcdebd8c3402778e
Roshan Shrestha <roshanshrestha01@gmail.com>
2021-10-29 10:28:41 +0545

    Minor fix to check address in create orders

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,5 +1,5 @@
 function createOrder(order) {
-    if (!order || !order.pizzaId || order.address) {
+    if (!order || !order.pizzaId || !order.address) {
         throw new Error('To order pizza please provide pizza type and address where pizza should be delivered');
     }
     return {}
