49364af825fb23ac2692e35281fd2d387410cb41
Roshan Shrestha <roshanshrestha01@gmail.com>
2021-10-29 14:58:56 +0545

    Update and delete order from dynamodb

--- handlers/update-order.js
+++ handlers/update-order.js
@@ -1,10 +1,29 @@
-function updateOrder(id, updates) {
-    if (!id || !updates) {
-        throw new Error("Order id and updates objects are required for updating order.")
-    }
-    return {
-        message: `Order ${id} was successfully updated.`
-    }
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+function updateOrder(orderId, options) {
+    if (!options || !options.pizza || !options.address)
+        throw new Error('Both pizza and address are required to update an order')
+    return docClient.update({
+        TableName: 'pizza-orders',
+        Key: {
+            orderId: orderId
+        },
+        UpdateExpression: 'set pizza = :p, address=:a',
+        ExpressionAttributeValues: {
+            ':p': options.pizza,
+            ':a': options.address
+        },
+        ReturnValues: 'ALL_NEW'
+    }).promise()
+        .then((result) => {
+            console.log('Order is updated!', result)
+            return result.Attributes
+        })
+        .catch((updateError) => {
+            console.log(`Oops, order is not updated :(`, updateError)
+            throw updateError
+        })
 }
 
 module.exports = updateOrder;
